a506bced9f824a926b95b7ca8a79dde86315bc8f
Sam <samuelrichardson902@gmail.com>
2025-07-19 22:04:32 +0100

    changed to static export

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
